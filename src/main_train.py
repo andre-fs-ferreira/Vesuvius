e74b1af327405bf647c9192567e57fff83117e00
ShadowTwin41 <andre_mocho_10@hotmail.com>
2026-01-13 18:42:30 +0000

    correct loss functions and load

--- src/main_train.py
+++ src/main_train.py
@@ -1,6 +1,6 @@
 import json
 import sys
-sys.path.append("/mounts/disk4_tiago_e_andre/vesuvius/Vesuvius/utils")
+sys.path.append("../utils")
 from main_train_class import main_train_STU_Net
 
 # Warning Suppressions (Must be at the top)
@@ -10,6 +10,6 @@ warnings.filterwarnings("ignore", category=UserWarning, module="zarr.codecs.numc
 if __name__=="__main__":
     CONFIG_FILE = "../configs/main_second_step.json"
     with open(CONFIG_FILE, "r") as f:
-        pretrain_config = json.load(f)
-    trainer = main_train_STU_Net(pretrain_config)
+        second_step_config = json.load(f)
+    trainer = main_train_STU_Net(second_step_config)
     trainer.train_loop()
